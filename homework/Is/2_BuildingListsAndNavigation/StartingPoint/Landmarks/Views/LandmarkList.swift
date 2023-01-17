205286772a256ab12afa1acf1f327e65570c2800
sei <vanism7557@gmail.com>
2023-01-17 23:17:41 +0900

    study: ch2-sec4 Create the List of Landmarks

---
+++ homework/Is/2_BuildingListsAndNavigation/StartingPoint/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by sei on 2023/01/17.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
