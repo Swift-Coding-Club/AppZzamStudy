9173195c9c8b2491d56af300a4f5894b4ccf132c
sei <vanism7557@gmail.com>
2023-01-17 23:39:56 +0900

    study: ch2-sec7 Pass Data into Child Views

--- homework/Is/2_BuildingListsAndNavigation/StartingPoint/Landmarks/Views/MapView.swift
+++ homework/Is/2_BuildingListsAndNavigation/StartingPoint/Landmarks/Views/MapView.swift
@@ -1,26 +1,34 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+ See LICENSE folder for this sample’s licensing information.
 
-Abstract:
-A view that presents a map.
-*/
+ Abstract:
+ A view that presents a map.
+ */
 
 import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
+    var coordinate: CLLocationCoordinate2D
+    @State private var region = MKCoordinateRegion()
 
     var body: some View {
         Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
+    }
+
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
-    }
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
+           }
 }
